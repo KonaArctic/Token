b4b5c68c10ee8b86c5dd9568a4340e643c06fbc7
Kona Arctic <kona@kona.akona.me>
2021-08-21 14:27:21 -0400

    command-line generator

---
+++ cli/main.go
@@ -0,0 +1,34 @@
+// Public Access Client. Copyright (c) 2021 Kona Arctic. All rights reserved. ABSOLUTELY NO WARRANTY! https://akona.me mailto:arcticjieer@gmail.com
+// HTTP CONNECT proxy server
+package main
+import "flag"
+import "gitea.kona.cf/Kona/Token"
+import "os"
+
+func main( ) {
+	var err error
+	var token token.Token
+	os.Stderr.Write( [ ]byte( "Copyright (c) 2021 Kona Arctic. All rights reserved. ABSOLUTELY NO WARRANTY! https://akona.me mailto:arcticjieer@gmail.com\r\n" ) )
+
+	// Parse arguments
+	flag.Usage = func( ) {
+		flag.CommandLine.Output( ).Write( [ ]byte( "For help and usage please contact software provider.\r\n" ) ) }
+	service := flag.Uint( "service" , 0 , "" )
+	flag.Uint64Var( & token.Ident , "ident" , 1 , "" )
+	flag.Int64Var( & token.Expire , "expire" , 1 * 60 * 60 * 24 , "" )
+	ladder := flag.Uint( "ladder" , 2 , "" )
+	flag.Parse( )
+	token.Service = uint16( * service )
+	token.Ladder = uint32( * ladder )
+
+	// Make token
+	_ , err = os.Stdout.Write( [ ]byte( token.String( ) + "\r\n" ) )
+	if err != nil {
+		os.Stderr.Write( [ ]byte( "Fatal: " + err.Error( ) + "\r\n" ) )
+		os.Exit( 1 )
+	}
+
+	return
+}
+
+
